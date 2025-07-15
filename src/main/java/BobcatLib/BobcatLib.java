053be6cab9bf0d257ef342c7b4a3f52aaaaf2311
aopalka <79727669+aopalka@users.noreply.github.com>
2025-07-15 00:10:15 -0400

    Test

---
+++ src/main/java/BobcatLib/BobcatLib.java
@@ -0,0 +1,9 @@
+package BobcatLib;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class BobcatLib {
+    public BobcatLib(){
+    SmartDashboard.putString("Library", "BobcatLib");
+    }
+}
